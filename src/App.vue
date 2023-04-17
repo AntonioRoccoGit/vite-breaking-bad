2043b3aff3f39c5613efe3093fc8be1215708f13
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-04-17 16:26:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script>
+import AppCard from './components/AppCard.vue';
+import axios from "axios";
+import { store } from "./store";
+export default {
+  data() {
+    return {
+      store,
+      manyCard: 0
+    }
+  },
+  components: {
+    AppCard
+  },
+  mounted() {
+    axios.get(store.getUrl).then((resp) => {
+      this.store.cardsArray = resp.data.data;
+    }),
+      axios.get(store.getUrl).then((resp) => {
+        this.manyCard = resp.data.meta;
+      })
+  }
+}
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="p-2">Yu-Gi-Oh Api</h1>
+
+  </header>
+
+  <body>
+    <section class="select p-4">
+      <select class="rounded-1" name="card-type" id="">
+        <option value="0">Alien</option>
+      </select>
+    </section>
+
+    <section class="selected-card ">
+      <div class="container">
+        <h3 class="founded"> Founded <span>{{ manyCard.current_rows }}</span> cards</h3>
+        <div class="row row-cols-5 gx-4 gy-2">
+          <div class="col" v-for="(item, index) in store.cardsArray">
+            <AppCard :cards="store.cardsArray[index]" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</template>
+
+<style lang="scss">
+@use "./scss/general.scss"
+</style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: {
+        cards: Object
+    }
+}
+</script>
+
+<template>
+    <div class="ms_card">
+        <img :src="cards.card_images[0].image_url" alt="">
+        <h2>{{ cards.name }}</h2>
+        <span class="type">{{ cards.archetype }}</span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../scss/partials/variables.scss" as *;
+
+.ms_card {
+    background-color: $bg_main_color;
+    text-align: center;
+    min-height: 100%;
+
+    h2 {
+        font-size: .8rem;
+        font-weight: 700;
+        color: white;
+        text-transform: uppercase;
+    }
+}
+</style>
